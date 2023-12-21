1ddbe13faf9a845f5348d30b538f922128889192
Izumi Kiuchi <izumi.kiuchi@i-zuna.co.jp>
2023-12-21 20:34:48 +0900

    update label

--- screenTest.py
+++ screenTest.py
@@ -14,10 +14,10 @@ UUID = "aaaaaaaa-bbbb-bbbb-bbbb-bbbbbbbbbbbb"
 
 
 
-async def maincentral():
+async def maincentral(device):
     # Scan device
-    print("main")
-    device = await scan('TEST BLE')
+    #print("main")
+    #device = await scan('TEST BLE')
     #print('found', device.name, device.address)
 
     async with BleakClient(device, timeout=None) as client:
@@ -49,20 +49,23 @@ async def scan(prefix='TEST BLE'):
             print('continue..')
             continue
 
+
+
 def update_label_after_scan():
+    label.config(text="接続中...")
+    button.config(state=tk.DISABLED)
     device = asyncio.run(scan())
-    print("見つかった！")
-    label.config(text="ババババb")
-
-
+    label.config(text="接続完了")
+    button.config(state=tk.NORMAL)
+    asyncio.run(maincentral(device))
+   
    
 
 def on_button_click():
-    print("接続中")
-    #asyncio.run(scan())
     thread1 = threading.Thread(target=update_label_after_scan)
-    thread1.start() 
-    #終わったら何か表示させる
+    thread1.start()
+    
+
    
 
 # メインウィンドウを作成
@@ -71,11 +74,11 @@ root.title("iPad5重音キーボード")
 root.geometry("500x300")
 
 # ボタンを作成
-button = tk.Button(root, text="非同期処理を開始", command=on_button_click)
+button = tk.Button(root, text="デバイスに接続する", command=on_button_click)
 button.pack(pady=10)
 
 # ラベルを作成
-label = tk.Label(root, text="ボタンをクリックしてください。")
+label = tk.Label(root, text="接続を開始するにはボタンをクリックしてください")
 label.pack(pady=10) #paddingを設定
 
 # イベントループを開始
