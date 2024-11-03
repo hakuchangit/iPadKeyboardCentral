b0357b036010c6ce08d9ddff124439055561140d
Izumi Kiuchi <izumi.kiuchi@i-zuna.co.jp>
2024-11-03 16:15:32 +0900

    change uuid

--- central.py
+++ central.py
@@ -5,8 +5,8 @@ from bleak import BleakScanner
 import time
 
 # mac_address = "76:95:E3:BB:43:7B"
-CHARACTERISTIC_UUID = "aaaaaaaa-dddd-bbbb-bbbb-bbbbbbbbbbbb"
-UUID = "aaaaaaaa-bbbb-bbbb-bbbb-bbbbbbbbbbbb"
+CHARACTERISTIC_UUID = "C2CF9284-8903-4EA0-A873-FE3AB1A56FE8"#NotifyCharacteristic
+UUID = "aaaaaaaa-bbbb-bbbb-bbbb-bbbbbbbbbbbb" #WriteWithoutResponseCharacteristic
 
 async def scan(prefix='TEST BLE'):
     while True:
@@ -15,7 +15,7 @@ async def scan(prefix='TEST BLE'):
             devices = await BleakScanner.discover()
             for d in devices:
                 print(f"address: {d.address}, name: {d.name}, uuid: {d.metadata['uuids']}")
-                if d.name == 'TEST BLE' or d.metadata['uuids'] == ['aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee']:
+                if d.name == 'TEST BLE' or d.metadata['uuids'] == ['ACDD196E-C057-4133-AFD8-18E10378BFEB']:#BLEServiceUUID
                     return d
         except StopIteration:
             print('continue..')
@@ -33,8 +33,8 @@ async def main():
     #print('found', device.name, device.address)
 
     async with BleakClient(device, timeout=None) as client:
-        x = await client.write_gatt_char(UUID,b"\0x01")
-        print("Connected: {0}".format(x))
+        # x = await client.write_gatt_char(UUID,b"\0x01")
+        # print("Connected: {0}".format(x))
        # await client.start_notify(CHARACTERISTIC_UUID, notification_handler)
 
         await client.start_notify(CHARACTERISTIC_UUID,  notify_izunya)
